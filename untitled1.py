defed99f4941ded4f6c6f35e7195b8c0a3075783
dbanzz <dzn508@york.ac.uk>
2024-01-17 09:41:33 +0800

    Bar chart

--- untitled1.py
+++ untitled1.py
@@ -28,3 +28,26 @@ plt.ylabel('Number of Vehicles')
 plt.grid(True)
 plt.legend(title='Electric Vehicle Type', loc='upper left')
 plt.show()
+
+
+#Group the data and then count the quantity, then install the year grouping to select the top ten manufacturers of each year, and finally reshape the data, with year as the row and manufacturer as the column.
+tmpy = EVPD_sampled.groupby(['Model Year', 'Make']).size().groupby(level=0, group_keys=False).nlargest(10).unstack()
+
+#Set appropriate graph size
+fig, ax1 = plt.subplots(figsize=(15, 8))
+
+#Draw a Bar chart(Shows the annual changes in the number of electric vehicles registered by the top 10 manufacturers)
+tmpy.plot(kind='bar', stacked=True, ax=ax1, colormap='tab20', alpha=0.8)
+
+#Set labels and titles
+ax1.set_xlabel('Model Year')
+ax1.set_ylabel('Number of Vehicles(Top 10)')
+ax1.set_title('Top 10 Electric Vehicle Makes Market Share by Year')
+
+#Put legends on the upper left
+ax1.legend(loc='upper left')
+
+#Show bar chart
+plt.xticks(rotation=0)
+plt.grid(True)
+plt.show()
